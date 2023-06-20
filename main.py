2ccb91575c05ec54e258dbf7505aa91bc72c7b55
kuj1 <the.same.dentist@gmail.com>
2023-06-21 00:58:22 +0300

    ref code

--- main.py
+++ main.py
@@ -3032,11 +3032,7 @@ def grab_data(req, monitors=False, mice=False, ddr=False, cartridges=False):
                                 except:
                                     print('\t[-] Image not found')
 
-                                try:
-                                    char_item_wrap = char_item_soup.find('div', attrs={'id': 'tabs_description'}).find('ul', class_='featureList').find_all('li', class_='featureList__item')
-
-                                except Exception as ex:
-                                    print(ex)
+                                char_item_wrap = char_item_soup.find('div', attrs={'id': 'tabs_description'}).find('ul', class_='featureList').find_all('li', class_='featureList__item')
 
                                 try:
                                     for char_item in char_item_wrap:
